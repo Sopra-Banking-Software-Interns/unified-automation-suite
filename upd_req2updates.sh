45fa60e5ebce75909e37115c1efa3247fe49bb9a
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-27 11:15:40 +0530

    Update upd_req2updates.sh

--- upd_req2updates.sh
+++ upd_req2updates.sh
@@ -1,10 +1,11 @@
+# Checks if UPD tag is found in the latest comment or not; if found, it creates the file with needed updates
 touch updates.txt
 file="update_requirement.txt"
 check=0
 while IFS= read -r line || [ -n "$line" ]; do
 line=$(echo "$line" | awk '{$1=$1};1')
   if [[ "$line" == "~UPD" && $check -eq 0 ]]; then
-    echo "Flag ~UPD found in the first line. Performing actions..."
+    echo "Flag UPD found in the first line. Performing actions..."
     check=1
   elif [[ $check -eq 0 ]]; then
     echo "Flag ~UPD not found. Aborting..."
