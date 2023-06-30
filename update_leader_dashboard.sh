50c8a87deb847acbcdf562c93d5e3d9420fab624
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-30 17:13:48 +0530

    Update update_leader_dashboard.sh

--- update_leader_dashboard.sh
+++ update_leader_dashboard.sh
@@ -2,8 +2,8 @@ current_sha=$(curl -s -u "Riyu44:$token" "https://api.github.com/repos/Sopra-Ban
 echo $current_sha
 curl -X PUT -u "Riyu44:$token" \
   -H "Accept: application/vnd.github+json" \
-  -d '{
+  -d "{
     "message": "workflow2",
     "content": "",
     "sha": "$current_sha"
-  }' "https://api.github.com/repos/Sopra-Banking-Software-Interns/Sopra-Banking-Software-Interns.github.io/contents/text"
+  }" "https://api.github.com/repos/Sopra-Banking-Software-Interns/Sopra-Banking-Software-Interns.github.io/contents/text"
