cb9d6acad15533f44e5dcbb4ae4fe547b06bba21
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-30 16:49:23 +0530

    Update update_leader_dashboard.sh

--- update_leader_dashboard.sh
+++ update_leader_dashboard.sh
@@ -1,9 +1,9 @@
 GITHUB_USERNAME="Riyu44"
 GITHUB_TOKEN=$token
-curl -X POST -u "$GITHUB_USERNAME:$GITHUB_TOKEN" \
+curl -L \
+  -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -d '{
-    "message": "Unified-automation-trigger",
-    "content": "update the index file",
-    "sha": "'"$current_sha"'"
-  }' "https://api.github.com/repos/Sopra-Banking-Software-Interns/Sopra-Banking-Software-Interns.github.io/contents/text"
+  -H "Authorization: Bearer $token"\
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/Sopra-Banking-Software-Interns/Sopra-Banking-Software-Interns.github.io/contents/text \
+  -d '{"message":"my commit message","committer":{"name":"Monalisa Octocat","email":"octocat@github.com"},"content":"bXkgbmV3IGZpbGUgY29udGVudHM="}'
