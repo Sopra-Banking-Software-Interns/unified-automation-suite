dd68c323f03f0975218747bb9359e95e918ff75f
Priyansh <100060048+Riyu44@users.noreply.github.com>
2023-06-30 16:34:56 +0530

    Update update_packages.sh

--- update_packages.sh
+++ update_packages.sh
@@ -45,10 +45,3 @@ curl -X PUT -u "$GITHUB_USERNAME:$GITHUB_TOKEN" \
     "content": "'"$encoded_package_json"'",
     "sha": "'"$current_sha"'"
   }' "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/contents/$PACKAGE_JSON_FILE"
-curl -X PUT -u "$GITHUB_USERNAME:$GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github+json" \
-  -d '{
-    "message": "Unified-automation-trigger",
-    "content": "update the index file",
-    "sha": "'"$current_sha"'"
-  }' "https://api.github.com/repos/Sopra-Banking-Software-Interns/Sopra-Banking-Software-Interns.github.io/contents/text"
